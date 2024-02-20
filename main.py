73fe54c8e45b2aa369009b41f93d28e31bfe3594
Lil Lord <frududewa760@gmail.com>
2024-02-20 17:25:40 +0200

    віфвфів

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         try:
-            asyncio.run(main())
+            asyncio.run(start_main())
         except KeyboardInterrupt:
             pass
         finally:
