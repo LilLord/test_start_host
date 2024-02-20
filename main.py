c6f3cb273adc878ef0900ba62d848a6f11c71546
Lil Lord <frududewa760@gmail.com>
2024-02-20 16:32:59 +0200

    dd

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
-import asyncio
-import logging
-import sys
-import threading
-import time
 import streamlit as st
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+import asyncio
+import random as r
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = "6050835358:AAFhyCX-Sb2HNlajHr2VdV8XVpMLkA8I7BE"
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
+val_one = 0
+val_two = 0
 
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+st.title("Hello World")
+async def firstWorker():
+    global val_one
+    while True:
+        await asyncio.sleep(r.randint(1, 3))
+        val_one = r.randint(1, 10)
+        st.metric("First Worker Executed: ",val_one)
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+async def secondWorker():
+    global val_two
+    while True:
+        await asyncio.sleep(r.randint(1, 3))
+        val_two = r.randint(1, 10)
+        st.metric("Second Worker Executed: ",val_two)
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
 
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
-    await dp.start_polling(bot)
+async def main():
+    await(asyncio.gather(
+        firstWorker(),
+        secondWorker()
+            )
+        )
 
-def start():
-    asyncio.run(main())
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    with st.empty:
+if __name__ == '__main__':
+    with st.empty(): # Modified to use empty container
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-        loop.run_until_complete(dp.start_polling(bot, loop=loop))
-        loop.close()
+        try:
+            asyncio.run(main())
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print("Closing Loop")
+            loop.close()
